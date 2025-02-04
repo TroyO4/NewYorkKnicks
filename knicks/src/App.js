de5243a1423b59424deda30c2321d2ce8cf65d49
troy <oconnort@go.stockton.edu>
2025-02-04 16:30:12 -0500

    navbar

--- knicks/src/App.js
+++ knicks/src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import './App.css';
-import Header from "./funcComp/Header"; // Note the uppercase 'H' in Header
+import Navbar from './funcComp/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header /> {/* Correctly use the component */}
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+      </Router>
+    </>
   );
 }
 
